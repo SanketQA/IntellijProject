2b8889b064621d8fdfd2ae6d330612ee8b025580
Sanket <sanketpawar7979@gmail.com>
2022-05-22 22:13:12 +0530

    First Commit

---
+++ src/practice/Sample.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Sample
+{
+    public static void main(String[]args)
+{
+    System.out.println("hello world");
+    int a=10;
+    int b=20;
+    int c;
+    c=a+b;
+    System.out.println(c);
+
+
+}
+
+}
